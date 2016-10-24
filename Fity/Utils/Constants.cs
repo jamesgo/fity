19b28dab1887afecd096a629cfd61ca7b6ccca21
JamesGo <james.godish@gmail.com>
2016-10-24 01:24:08 -0700

    Add Bing maps view

---
+++ Fity/Utils/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fity.Utils
+{
+    public class Constants
+    {
+        internal const string BingMapsKey = "4F1L05rZr38cgVAVkeXF~ERq2R8zmtNmtLu_NhSOLmA~AiSKP9puLXT5SpIV0Nei7XsdCywiN_Z3TxRMwMvjnjAYHzpI1EwGqRnAVorMk7oY";
+    }
+}
